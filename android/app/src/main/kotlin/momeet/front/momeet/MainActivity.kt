37e8a92097aea54c5277d0c8749888882d61e8f4
Gam._.e <gam1017@kumoh.ac.kr>
2025-03-12 11:09:45 +0900

    START

---
+++ android/app/src/main/kotlin/momeet/front/momeet/MainActivity.kt
@@ -0,0 +1,5 @@
+package momeet.front.momeet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/momeet/frontend/momeet/MainActivity.kt
@@ -0,0 +1,5 @@
+package momeet.frontend.momeet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
